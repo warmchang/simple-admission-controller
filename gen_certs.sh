22d32ea76105a0024aecc1bd2bbee591dc3286ac
Chris Dyer <cdyer1980@gmail.com>
2019-07-06 16:43:14 -0400

    Tweaking secret generation logic.

--- gen_certs.sh
+++ gen_certs.sh
@@ -14,10 +14,10 @@ openssl req -new -x509 -key certs/ca.key -out certs/ca.crt -config certs/ca_conf
 openssl genrsa -out certs/simple-key.pem 2048
 
 # CREATE A CSR FROM THE CONFIGURATION FILE AND OUR PRIVATE KEY
-openssl req -new -key certs/simple-key.pem -subj "/CN=simple.default.svc" -out simple.csr -config certs/simple_config.txt
+openssl req -new -key certs/simple-key.pem -subj "/CN=simple.default.svc" -out certs/simple.csr -config certs/simple_config.txt
 
 # CREATE THE CERT SIGNING THE CSR WITH THE CA CREATED BEFORE
-openssl x509 -req -in simple.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/simple-crt.pem
+openssl x509 -req -in certs/simple.csr -CA certs/ca.crt -CAkey certs/ca.key -CAcreateserial -out certs/simple-crt.pem
 
 # INJECT CA IN THE WEBHOOK CONFIGURATION
 export CA_BUNDLE=$(cat certs/ca.crt | base64 | tr -d '\n')
